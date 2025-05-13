46bfa574c01ae4963b3dd9f78075211432a2bb40
Oleg Koretsky <oleg@radiokot.com.ua>
2025-05-13 21:18:35 +0300

    Revert "Enable bottom sheet dismiss by a swipe"
    This reverts commit d767701db8d4b67a715f469c88a311d42b236f7e.
    
    # Conflicts:
    #	fastlane/metadata/android/en-US/changelogs/6.txt

--- app/src/main/java/ua/com/radiokot/money/Navigation.kt
+++ app/src/main/java/ua/com/radiokot/money/Navigation.kt
@@ -70,10 +70,10 @@ fun MoneyAppModalBottomSheetLayout(
     // Only do it if not navigating to another sheet.
     LaunchedEffect(bottomSheetNavigator) {
         var lastShownBackStackEntry: NavBackStackEntry? = null
-        snapshotFlow { bottomSheetNavigator.navigatorSheetState.currentValue }
-            .collect { sheetState ->
+        snapshotFlow { bottomSheetNavigator.navigatorSheetState.targetValue }
+            .collect { targetSheetState ->
                 val currentBackStackEntry = moneyAppNavController.currentBackStackEntry
-                if (sheetState != ModalBottomSheetValue.Hidden) {
+                if (targetSheetState != ModalBottomSheetValue.Hidden) {
                     lastShownBackStackEntry = currentBackStackEntry
                 } else if (currentBackStackEntry == lastShownBackStackEntry) {
                     moneyAppNavController.popBackStack()
@@ -87,6 +87,7 @@ fun MoneyAppModalBottomSheetLayout(
             topStart = 16.dp,
             topEnd = 16.dp,
         ),
+        sheetGesturesEnabled = false,
         content = {},
         modifier = Modifier
             .fillMaxSize()
